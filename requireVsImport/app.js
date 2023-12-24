c07eb2668e391021dc67ef9e1bbef197fe4319dd
osaydayoub <osaydayoub@gmail.com>
2023-12-24 23:03:46 +0200

    require vs import

---
+++ requireVsImport/app.js
@@ -0,0 +1,5 @@
+import { add,subtract,multiple2 } from './functions.js'
+
+console.log(add(10,20));
+console.log(subtract(10,20));
+console.log(multiple2(10));
---
+++ requireVsImport/functions.js
@@ -0,0 +1,11 @@
+export function add (a,b){
+    return a+b;
+}
+
+export function subtract(a,b){
+    return a-b;
+}
+
+export function multiple2(a){
+    return a*2;
+}
